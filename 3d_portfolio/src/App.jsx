dbf828974c265a1a0f39d378a1c8cd4b05f48563
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-06-29 01:10:32 +0500

    Structure Created

--- 3d_portfolio/src/App.jsx
+++ 3d_portfolio/src/App.jsx
@@ -1,9 +1,30 @@
+import {BrowserRouter} from 'react-router-dom';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
+
 const App = () => {
 
   return (
-    <div>
-      3D Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+
+        <div className="bg-hero-patterm bg-cover bg-no-repeat bg-center">
+          < Navbar />
+          < Hero />
+        </div>
+
+        < About />
+        < Experience />
+        < Tech />
+        < Works />
+        < Feedbacks />
+
+        <div className="relative z-0">
+          < Contact />
+          < StarsCanvas />
+        </div>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
